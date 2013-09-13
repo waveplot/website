dfd4c58b48bc5a17056417e6b33069058514bdeb
Ben Ockmore <ben.sput@gmail.com>
2013-09-13 23:11:28 +0100

    Added copyright header to schema.py.

--- waveplot/schema.py
+++ waveplot/schema.py
@@ -1,12 +1,33 @@
+# -*- coding: utf8 -*-
+
+# Copyright 2013 Ben Ockmore
+
+# This file is part of WavePlot Server.
+
+# WavePlot Server is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# WavePlot Server is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with WavePlot Server. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function, absolute_import, division
+
+import uuid
+import base64
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy import Column, Integer, BigInteger, UnicodeText, Boolean, SmallInteger, BINARY, ForeignKey, Interval, String
 
-import uuid
-import base64
-
-from passwords import passwords
+from waveplot.passwords import passwords
 
 Base = declarative_base()
 Session = sessionmaker()
