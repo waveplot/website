9585eb6ce89473e5c38134926af3d41e7df56ab3
Ben Ockmore <ben.sput@gmail.com>
2014-09-05 23:17:28 +0100

    Updated dependency versions, fixed issue in moving to later angular-translate.

--- web/app/layout/layout.js
+++ web/app/layout/layout.js
@@ -2,6 +2,6 @@ function LayoutController($scope, $translate, $translatePartialLoader) {
     $translatePartialLoader.addPart('layout');
 
     $scope.changeLanguage = function (key) {
-        $translate.uses(key);
+        $translate.use(key);
     };
 }
