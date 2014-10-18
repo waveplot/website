c3e87c4c7fc2855017741b30492cd6e0f5d3eaf5
Ben Ockmore <ben.sput@gmail.com>
2014-10-18 18:41:30 +0100

    Removed non-frontend files which have now mostly been migrated to other repos.

--- run_tests.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
-
-import unittest
-
-if __name__ == '__main__':
-    suite = unittest.defaultTestLoader.discover("tests")
-    unittest.TextTestRunner(verbosity=2).run(suite)
